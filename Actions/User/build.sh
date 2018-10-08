22fb2f11eef7f34eb2f334250f7e1e90ff8973e5
William Melody <hi@williammelody.com>
2018-10-07 22:42:34 -0700

    Move User action building to a build.sh file in the action directory.

---
+++ Actions/User/build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+_build_username_actions() {
+  local _users_and_ids
+  _users_and_ids=($(dscl . list /Users uid | grep -v '^_'))
+
+  for __user in ${_users_and_ids[@]}
+  do
+    local _username
+    _username="$(echo "${__user}" | awk '{print $1}')"
+
+    # Only print users with valid home directories, which is used here as an
+    # indication that the account is a valid login account. So far I've been
+    # unable to find a better way to list only full login user accounts.
+    local _home_directory
+    _home_directory="$(
+      dscl . -read "/Users/${_username}" NFSHomeDirectory | \
+        awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}'
+    )"
+
+    # Skip system users
+    if [[ "${_username}" == "root" ]]         ||
+       [[ "${_username}" == "daemon" ]]       ||
+       [[ "${_home_directory}" =~ ^/var.* ]]  ||
+       [[ "${_home_directory}" =~ ^/dev.* ]]  ||
+       [[ "${_home_directory}" =~ ^/opt.* ]]
+    then
+      continue
+    fi
+
+    _create_action "user" "${_username}"
+  done
+} && _build_username_actions
---
+++ Actions/User/script.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+_switch_to_the_user_with_the_same_name_as_this_app_bundle() {
+  # Get username from the application bundle name.
+  local _username="$(
+    ps -o comm= $PPID \
+    | grep -oh '[^/]\+.app' \
+    | sed -e 's/.app//' \
+    | sed -e 's/MacOS//'
+  )"
+  local _user_id=$(id -u "${_username}")
+  local _dialog_message
+
+  if [[ -z "${_user_id:-}" ]]
+  then
+    read -r -d '' "_dialog_message" <<DIALOG_MESSAGE_HEREDOC
+No user named '${_username}' found. To recreate Actions run:
+  spotlight-actions
+DIALOG_MESSAGE_HEREDOC
+    osascript -e "display dialog \"${_dialog_message}\""
+  elif [[ "$(whoami)" == "${_username}" ]]
+  then
+    osascript -e "display dialog \"Already logged in to '${_username}'.\""
+  else
+    "/System/Library/CoreServices/Menu Extras/User.menu/Contents/Resources/CGSession" -switchToUserID "${_user_id}"
+  fi
+} && _switch_to_the_user_with_the_same_name_as_this_app_bundle
+
